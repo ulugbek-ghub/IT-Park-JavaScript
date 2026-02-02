47b06739596559214edb4fc8235a00ddd938e520
ulugbek-ghub <ulugbek.github@gmail.com>
2026-02-02 12:16:06 +0500

    Uyga Vazifa 30-darsniki

--- JavaScript/30-Dars/app.js
+++ JavaScript/30-Dars/app.js
@@ -14,19 +14,58 @@
 // const PersonOne = new Person('Gofur', 'Ruziev')
 // console.log(PersonOne);
 
-function Car(brand, model, year, color){
-    this.brand = brand
-    this.color = color
-    this.year = year
-    this.getInfo = function(){
-        console.log(`Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}, Color: ${this.color}`);
+// function Car(brand, model, year, color){
+//     this.brand = brand
+//     this.color = color
+//     this.year = year
+//     this.getInfo = function(){
+//         console.log(`Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}, Color: ${this.color}`);
+//     }
+// }
+
+// const carOne = new Car('BMW', 'm3', 2015, 'cobalt midnight')
+// const carTwo = new Car('Mercedes F1', 'W11', 2020, 'black')
+// const carThree = new Car('Lamborghini', 'Aventador', 2021, 'yellow')
+
+// console.log(carOne);
+// console.log(carTwo);
+// console.log(carThree);
+
+
+
+// UY ISHI
+
+const colorBtn = document.querySelector('#colorBtn');
+const dateBtn = document.querySelector('#dateBtn');
+const dateResult = document.querySelector('#dateResult');
+const textBtn = document.querySelector('#textBtn');
+const removeBtn = document.querySelector('#removeBtn');
+const userInput = document.querySelector('#userInput');
+const checkBtn = document.querySelector('#checkBtn');
+
+colorBtn.onclick = () => {
+    colorBtn.style.backgroundColor = 'gold';
+    colorBtn.style.color = 'black';
+};
+
+dateBtn.onclick = () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 5);
+    dateResult.textContent = "5 kundan keyin: " + date.toLocaleDateString();
+};
+
+textBtn.onclick = () => {
+    textBtn.textContent = "Yangi matn";
+};
+
+removeBtn.onclick = () => {
+    removeBtn.remove();
+};
+
+checkBtn.onclick = () => {
+    if (userInput.value.trim() === "") {
+        alert("bir narsa yoz");
+    } else {
+        alert('Rahmat');
     }
-}
-
-const carOne = new Car('BMW', 'm3', 2015, 'cobalt midnight')
-const carTwo = new Car('Mercedes F1', 'W11', 2020, 'black')
-const carThree = new Car('Lamborghini', 'Aventador', 2021, 'yellow')
-
-console.log(carOne);
-console.log(carTwo);
-console.log(carThree);
+};
