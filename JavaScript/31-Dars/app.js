39f6cb49be36229c45abb5a6df3f60879ba8dc6d
ulugbek-ghub <ulugbek.github@gmail.com>
2026-02-03 17:45:44 +0500

    React

---
+++ JavaScript/31-Dars/app.js
@@ -0,0 +1,14 @@
+// React - JS kutubxonasi, Framework emas
+
+// SPA (Single Page Application) tushunchasi - foydalanuvchi bitta sahifada qoladi, faqat kerakli qismlar JS orqali yangilanadi
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# IT-Park-JavaScript
-IT-Park JavaScript darslari
